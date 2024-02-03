041a1b0debd6f6af7fd68ba07e6248a7ce838b8a
vaibhavbkale <vaibhavkale5517@gmail.com>
2024-02-03 16:06:40 +0530

    VS Code first commit

---
+++ togglebox_color/README.md
@@ -0,0 +1,3 @@
+# togglebox_color
+
+A new Flutter project.
---
+++ togglebox_color/android/app/src/main/kotlin/com/example/togglebox_color/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.togglebox_color
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
